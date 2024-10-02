6ef995ab8f3438aaca28058575062c953c7125ca
jacomchan <161497096+jacomchan@users.noreply.github.com>
2024-10-02 13:00:50 +0000

    eeeeeeee

---
+++ js/random.js
@@ -0,0 +1,18 @@
+var button = document.getElementById("randomNumberButton")
+
+function randNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+  
+function generateNumber() {
+    let min = document.getElementById('minNumber').value;
+    let max = document.getElementById('maxNumber').value;
+    let rand = randNum(min,max);
+    document.getElementById("randomNumberOutput").innerHTML = rand;
+}
+
+button.addEventListener("click", function (e) {
+    generateNumber()
+});
